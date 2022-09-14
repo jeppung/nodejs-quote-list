bb4dc869d466f3b216283ded1e1855d3f4e4c2df
jeppung <jeppung@gmail.com>
2022-09-14 23:49:14 +0700

    Using asyncronous approach

--- app.js
+++ app.js
@@ -6,27 +6,38 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Input Name: ', (name) => {
-    rl.question('Input Quote: ', (quote) => {
-        if(fs.existsSync('./output.json') == false){
-            const arr = [];
-            arr.push({
-                name,
-                quote
-            });
-            fs.writeFileSync('./output.json', JSON.stringify(arr));
-            console.log('Data has been added!');
-        }else{
-            const file = fs.readFileSync('./output.json', 'utf-8');
-            const temp_arr = JSON.parse(file);
-            temp_arr.push({
-                name,
-                quote
-            });
-            fs.writeFileSync('./output.json', JSON.stringify(temp_arr));
-            console.log('Data has been added!');
-        }
-
-        rl.close();
+const pertanyaan = (q) => {
+    return new Promise((resolve, reject) => {
+        rl.question(`${q}: `, (value) => {
+            resolve(value);
+        });
     });
-});
+};
+
+const main = async () => {
+    const name = await pertanyaan('Input Name');
+    const quote = await pertanyaan('Input Quote');
+
+    if(fs.existsSync('./output.json') == false){
+        const arr = [];
+        arr.push({
+            name,
+            quote
+        });
+        fs.writeFileSync('./output.json', JSON.stringify(arr));
+        console.log('Data has been added!');
+    }else{
+        const file = fs.readFileSync('./output.json', 'utf-8');
+        const temp_arr = JSON.parse(file);
+        temp_arr.push({
+            name,
+            quote
+        });
+        fs.writeFileSync('./output.json', JSON.stringify(temp_arr));
+        console.log('Data has been added!');
+    }
+
+    rl.close();
+};
+
+main();
