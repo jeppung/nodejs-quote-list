089b304d4a09f59bc257f3b094b93984fdd80095
jeppung <jeppung@gmail.com>
2022-09-15 00:04:34 +0700

    Simplyfying code using local module

--- app.js
+++ app.js
@@ -1,43 +1,10 @@
-const fs = require('fs');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-const pertanyaan = (q) => {
-    return new Promise((resolve, reject) => {
-        rl.question(`${q}: `, (value) => {
-            resolve(value);
-        });
-    });
-};
+const {pertanyaan,saveData} = require('./system');
 
 const main = async () => {
     const name = await pertanyaan('Input Name');
     const quote = await pertanyaan('Input Quote');
 
-    if(fs.existsSync('./output.json') == false){
-        const arr = [];
-        arr.push({
-            name,
-            quote
-        });
-        fs.writeFileSync('./output.json', JSON.stringify(arr));
-        console.log('Data has been added!');
-    }else{
-        const file = fs.readFileSync('./output.json', 'utf-8');
-        const temp_arr = JSON.parse(file);
-        temp_arr.push({
-            name,
-            quote
-        });
-        fs.writeFileSync('./output.json', JSON.stringify(temp_arr));
-        console.log('Data has been added!');
-    }
-
-    rl.close();
+    saveData(name, quote);
 };
 
 main();
---
+++ system.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const pertanyaan = (q) => {
+    return new Promise((resolve, reject) => {
+        rl.question(`${q}: `, (value) => {
+            resolve(value);
+        });
+    });
+};
+
+const saveData = (name, quote) => {
+    if(fs.existsSync('./output.json') == false){
+        const arr = [];
+        arr.push({
+            name,
+            quote
+        });
+        fs.writeFileSync('./output.json', JSON.stringify(arr));
+        console.log('Data has been added!');
+    }else{
+        const file = fs.readFileSync('./output.json', 'utf-8');
+        const temp_arr = JSON.parse(file);
+        temp_arr.push({
+            name,
+            quote
+        });
+        fs.writeFileSync('./output.json', JSON.stringify(temp_arr));
+        console.log('Data has been added!');
+    }
+
+    rl.close();
+};
+
+module.exports = {
+    pertanyaan,
+    saveData
+}
